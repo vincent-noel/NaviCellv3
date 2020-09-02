b1161fd0bacc1f9db1373ae3f900563389b8752c
Vincent Noël <contact@vincent-noel.fr>
2020-09-02 12:01:20 +0200

    First commit

---
+++ src/main/java/fr/curie/navicell/NaviCellMap.java
@@ -0,0 +1,65 @@
+package fr.curie.navicell;
+
+import java.util.UUID;
+import java.nio.file.Path;
+import org.springframework.data.annotation.Id;
+import fr.curie.BiNoM.pathways.navicell.ProduceClickableMap;
+import fr.curie.BiNoM.pathways.wrappers.*;
+import fr.curie.navicell.storage.StorageException;
+import fr.curie.navicell.storage.StorageService;
+
+import org.sbml.x2001.ns.celldesigner.SbmlDocument;
+import org.sbml.x2001.ns.celldesigner.CelldesignerPaintDocument;
+
+public class NaviCellMap {
+
+  @Id
+  public String id;
+
+  public String folder;
+  public String name;
+  public String networkPath;
+  public String imagePath;
+  public String url;
+  // public String configPath;
+  
+  private boolean createFolder(StorageService storage) {
+    this.folder = UUID.randomUUID().toString();
+    try {
+      storage.createFolder(this.folder);  
+      return true;
+    }
+    catch (StorageException e) {
+      return false;
+    }
+  }
+  public NaviCellMap() {
+    
+  }
+  
+  public NaviCellMap(StorageService storage) {
+    
+    boolean folder_created = this.createFolder(storage);
+    while (!folder_created) {
+      folder_created = this.createFolder(storage);
+    }
+  }
+
+  // public NaviCellMap(String name, String network_path, String image_path, String config_path) {
+  //   this.name = name;
+  //   this.networkPath = network_path;
+  //   this.imagePath = image_path;
+  //   this.configPath = config_path;
+    
+  //   SbmlDocument cd = CellDesigner.loadCellDesigner(network_path);
+  //   // cd.getSbml().getModel().
+  // }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "{'id': '%s', 'folder': '%s', 'name': '%s', 'network_path': '%s', 'image_path': '%s'}",
+        id, folder, name, networkPath, imagePath);
+  }
+
+}
---
+++ src/main/java/fr/curie/navicell/storage/StorageService.java
@@ -0,0 +1,26 @@
+package fr.curie.navicell.storage;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+import java.util.List;
+
+public interface StorageService {
+
+	void init();
+
+	Path store(MultipartFile file, String folder, String filename);
+
+	void createFolder(String folder);
+	
+	Stream<Path> loadAll();
+	
+	Path load(String filename);
+
+	Resource loadAsResource(String filename);
+
+	void deleteAll();
+
+}
